1d03f8329e3797d3cb91fc48794ebb40ec895cf8
yasoobkhalid1 <yasoobkhalid1@gmail.com>
2022-01-04 09:51:42 -0800

    update introduction, add buttons

--- components/Introduction.js
+++ components/Introduction.js
@@ -2,24 +2,17 @@ import Image from "next/image";
 
 export default function Introduction() {
   return (
-    <div className="my-72 text-center">
+    <div className="my-72 text-center font-slab">
       <div className="">
         <h1 className="tracking-[-0.1em] font-medium drop-shadow-2xl">
           <span className="text-8xl pr-4 leading-normal">Hi. </span>
           <span className="text-8xl">
-            I'm{" "}
-            <span
-              className="font-semibold text-transparent 
-            bg-clip-text bg-gradient-to-t from-rose-500 to-rose-700"
-            >
-              Yasoob
-            </span>
-            .
+            I'm <span className="font-semibold bg-rose-500 px-8">Yasoob</span>.
           </span>
         </h1>
 
         <div
-          className="tracking-tight leading-loose text-xl
+          className="leading-loose text-xl
           text-slate-700 drop-shadow-2xl pt-8 px-30"
         >
           I'm a Full Stack Software Engineer, currently working on{" "}
@@ -35,25 +28,34 @@ export default function Introduction() {
           font-medium decoration-blue-500"
           >
             Google
-          </span>{" "}
-          . <br />
-          Previously, I have interned at{" "}
-          <span
-            className="underline underline-offset-4 decoration-[3px] 
-          font-medium decoration-emerald-500"
-          >
-            RePlate
-          </span>{" "}
-          and{" "}
-          <span
-            className="underline underline-offset-4 decoration-[3px] 
-          font-medium decoration-purple-500"
-          >
-            Walmart Labs
           </span>
-          . <br />
-          {/* TODO: Fill out contact me button */}
-          Contact Me
+          .
+          <br />I love to read and have recently gotten into{" "}
+          <span
+            className="underline underline-offset-4 decoration-[3px] 
+          font-medium decoration-green-500"
+          >
+            writing
+          </span>{" "}
+          about cool stuff that interests me.
+        </div>
+        <div className="flex justify-evenly">
+          <a
+            href="mailto:yasoobkhalid1@gmail.com"
+            className="mt-36 font-mono text-2xl border-4 border-black 
+            inline-block p-3 px-6 bg-amber-400 hover:bg-white 
+            ease-in duration-200"
+          >
+            Contact Me
+          </a>
+          <a
+            href="https://blog.yasoobk.com"
+            className="mt-36 font-mono text-2xl border-4 border-black 
+            inline-block p-3 px-6 bg-emerald-400 hover:bg-white 
+            ease-in duration-200"
+          >
+            Blog
+          </a>
         </div>
       </div>
     </div>

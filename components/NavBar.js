90c09c5698040a57265440eb90ef072f4437913b
yasoobkhalid1 <yasoobkhalid1@gmail.com>
2022-01-04 08:54:51 -0800

    add underline animation to navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,29 +1,31 @@
 import Link from "next/link";
 
 export default function NavBar() {
-  const sections = ["Education", "Skills", "Experience", "Projects", "Resume"];
+  const sections = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "#About" },
+    { name: "Education", link: "#Education" },
+    { name: "Experience", link: "#Experience" },
+    { name: "Projects", link: "#Projects" },
+    { name: "Contact", link: "#Contact" },
+    { name: "Resume", link: "#Resume" },
+  ];
   const links = sections.map((section) => {
     return (
-      <Link href={"#" + section}>
-        <a className="text-slate-600 hover:text-rose-500 hover:underline hover:underline-offset-4">
-          {section}
+      <Link href={section.link}>
+        <a
+          className="group-hover:opacity-50 group-hover:hover:opacity-100 
+          ease-in duration-100 relative before:absolute before:w-0 before:h-1 
+          before:-bottom-1 before:left-0 before:bg-red-500 before:invisible 
+          before:transition-all before:delay-100 before:ease-in-out
+          before:hover:visible before:hover:w-full
+        "
+        >
+          {section.name}
         </a>
       </Link>
     );
   });
 
-  const logo = (
-    <Link href="">
-      <a>YKN</a>
-    </Link>
-  );
-
-  return (
-    <div className="flex justify-between py-8 text-lg">
-      <div className="text-slate-600 hover:text-rose-400  hover:underline hover:underline-offset-4">
-        {logo}
-      </div>
-      <div className="flex space-x-10">{links}</div>
-    </div>
-  );
+  return <div className="my-8 text-lg flex justify-between group">{links}</div>;
 }
